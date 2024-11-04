7ebebb357105be9c4357a88fea8144c25a5f3a8a
Gwendal <gwendalferlin20@gmail.com>
2024-11-04 14:46:09 +0100

    pdf11 finito

--- src/Controller/FichierController.php
+++ src/Controller/FichierController.php
@@ -2,22 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Fichier;
+use App\Form\FichierType;
+use App\Repository\ScategorieRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Form\FichierType;
-use App\Entity\Fichier;
-use App\Repository\UserRepository;
-use App\Repository\ScategorieRepository;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Request;
-
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class FichierController extends AbstractController
 {
     #[Route('/ajout-fichier', name: 'app_ajout_fichier')]
     public function ajoutFichier(Request $request, ScategorieRepository $scategorieRepository,
-        EntityManagerInterface $em): Response {
+        EntityManagerInterface $em, SluggerInterface $slugger): Response {
         $fichier = new Fichier();
         $scategories = $scategorieRepository->findBy([], ['categorie' => 'asc', 'numero' => 'asc']);
         $form = $this->createForm(FichierType::class, $fichier, ['scategories' => $scategories]);
@@ -26,9 +26,28 @@ class FichierController extends AbstractController
             $selectedScategories = $form->get('scategories')->getData();
             foreach ($selectedScategories as $scategorie) {
                 $fichier->addScategory($scategorie);
+            }$file = $form->get('fichier')->getData();
+
+            if ($file) {
+                $nomFichierServeur = pathinfo($file->getClientOriginalName(),
+                    PATHINFO_FILENAME);
+                $nomFichierServeur = $slugger->slug($nomFichierServeur);
+                $nomFichierServeur = $nomFichierServeur . '-' . uniqid() . '.' . $file->guessExtension();
+                try {
+                    $fichier->setNomServeur($nomFichierServeur);
+                    $fichier->setNomOriginal($file->getClientOriginalName());
+                    $fichier->setDateEnvoi(new \Datetime());
+                    $fichier->setExtension($file->guessExtension());
+                    $fichier->setTaille($file->getSize());
+                    $em->persist($fichier);
+                    $em->flush();
+                    $file->move($this->getParameter('file_directory'), $nomFichierServeur);
+                    $this->addFlash('notice', 'Fichier envoyé');
+                    return $this->redirectToRoute('app_ajout_fichier');
+                } catch (FileException $e) {
+                    $this->addFlash('notice', 'Erreur d\'envoi');
+                }
             }
-            $em->persist($fichier);
-            $em->flush();
         }
 
         return $this->render('fichier/ajout-fichier.html.twig', [
@@ -48,8 +67,8 @@ class FichierController extends AbstractController
     #[Route('/liste-fichiers-par-utilisateur', name: 'app_liste_fichiers_par_utilisateur')]
     public function listeFichiersParUtilisateur(UserRepository $userRepository): Response
     {
-    $users = $userRepository->findBy([], ['name'=>'asc', 'prenom'=>'asc']);
-    return $this->render('fichier/liste-fichiers-par-utilisateur.html.twig', ['users'=>$users]);
+        $users = $userRepository->findBy([], ['name' => 'asc', 'prenom' => 'asc']);
+        return $this->render('fichier/liste-fichiers-par-utilisateur.html.twig', ['users' => $users]);
     }
-   
+
 };
--- src/Form/FichierType.php
+++ src/Form/FichierType.php
@@ -9,17 +9,33 @@ use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class FichierType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('fichier', FileType::class, array('label' => 'Fichier', 'mapped' => false, 'attr' => ['class' =>
+                'form-control'], 'label_attr' => ['class' => 'fw-bold'], 'constraints' => [
+                new File([
+                    'maxSize' => '2000k',
+                    'mimeTypes' => [
+                        'application/pdf',
+                        'application/x-pdf',
+                        'image/jpeg',
+                        'image/png',
+                    ],
+                    'mimeTypesMessage' => 'Le site accepte uniquement les fichiers PDF, PNG et JPG',
+                ]),
+            ]))
+
             ->add('nomOriginal', TextType::class, ['attr' => ['class' => 'form-control'], 'label_attr' =>
                 ['class' => 'fw-bold']])
             ->add('nomServeur', TextType::class, ['attr' => ['class' => 'form-control'], 'label_attr' =>
